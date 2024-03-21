20560eff2e0211fe2c7868727bfec88efc5cfbc8
Till <moldenhauer.till@gmail.com>
2024-03-21 13:28:55 +0100

    obsidian title

--- nvim/init.lua
+++ nvim/init.lua
@@ -96,6 +96,35 @@ require("obsidian").setup({
   --  * "notes_subdir" - put new notes in the default notes subdirectory.
   new_notes_location = "current_dir",
 
+  -- Optional, customize how note IDs are generated given an optional title.
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return suffix
+  end,
+
+  -- Optional, customize how note file names are generated given the ID, target directory, and title.
+  ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+  ---@return string|obsidian.Path The full path to the new note.
+  note_path_func = function(spec)
+    -- This is equivalent to the default behavior.
+    local path = spec.dir / tostring(spec.id)
+    return path:with_suffix(".md")
+  end,
+
 
   -- Optional, customize how markdown links are formatted.
   markdown_link_func = function(opts)
@@ -136,4 +165,7 @@ require("obsidian").setup({
       return string.format("![%s](%s)", path.name, path)
     end,
   },
+
+  ui = {enable=false}
+
 })
