5401cc5c01b8b3232bb5b34ef8f32b0e19085338
Till <moldenhauer.till@gmail.com>
2024-03-21 11:51:56 +0100

    obsidian config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+-- Plugins
+
 local vim = vim
 local Plug = vim.fn['plug#']
 
@@ -18,10 +20,12 @@ Plug('epwalsh/obsidian.nvim')
 
 vim.call('plug#end')
 
+-- Colorscheme
 vim.opt.termguicolors = true
 vim.cmd("colorscheme rose-pine")
 vim.wo.number = true
 
+-- Keymaps
 
 vim.g.mapleader=" "
 
@@ -33,6 +37,8 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-N>',':bnext<CR>')
 vim.keymap.set('n', '<C-P>',':bprev<CR>')
 vim.keymap.set('n','<leader>nt',':NERDTree<CR>')
+
+-- Tabline config
 
 vim.o.showtabline=2
 --vim.g.lightline = {colorscheme = 'rosepine'}
@@ -53,3 +59,81 @@ vim.g['lightline'] = {
     buffers = 'tabsel'
   }
 }
+
+-- Obsidian config
+require("obsidian").setup({
+
+  workspaces = {
+    {
+      name = "personal",
+      path = "~/obsidian/glasshouse",
+    },
+  },
+
+
+
+  -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+  -- way then set 'mappings = {}'.
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+  },
+
+  -- Where to put new notes. Valid options are
+  --  * "current_dir" - put new notes in same directory as the current buffer.
+  --  * "notes_subdir" - put new notes in the default notes subdirectory.
+  new_notes_location = "current_dir",
+
+
+  -- Optional, customize how markdown links are formatted.
+  markdown_link_func = function(opts)
+    return require("obsidian.util").markdown_link(opts)
+  end,
+
+  -- Either 'wiki' or 'markdown'.
+  preferred_link_style = "markdown",
+
+
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = "telescope.nvim",
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    mappings = {
+      -- Create a new note from your query.
+      new = "<C-x>",
+      -- Insert a link to the selected note.
+      insert_link = "<C-l>",
+    },
+  },
+
+  -- Specify how to handle attachments.
+  attachments = {
+    -- The default folder to place images in via `:ObsidianPasteImg`.
+    -- If this is a relative path it will be interpreted as relative to the vault root.
+    -- You can always override this per image by passing a full path to the command instead of just a filename.
+    img_folder = "assets/imgs",  -- This is the default
+    -- A function that determines the text to insert in the note when pasting an image.
+    -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+    -- This is the default implementation.
+    ---@param client obsidian.Client
+    ---@param path obsidian.Path the absolute path to the image file
+    ---@return string
+    img_text_func = function(client, path)
+      path = client:vault_relative_path(path) or path
+      return string.format("![%s](%s)", path.name, path)
+    end,
+  },
+})
